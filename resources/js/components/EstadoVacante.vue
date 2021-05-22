b54dac1040b2fda35b291672f341f0aa67e6e179
ssanmap <sebastian.smp21@gmail.com>
2021-05-21 20:26:59 -0400

    vue

--- resources/js/components/EstadoVacante.vue
+++ resources/js/components/EstadoVacante.vue
@@ -1,11 +1,43 @@
 <template>
     <div>
-
+            <span @click="cambiarEstado"
+            :class="claseEstadoVacante()"
+            :key="estadoVacanteData"
+            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full ">
+                    {{estadoVacante}}
+            </span>
     </div>
 </template>
 
 <script>
     export default {
+        props:['estado', 'vacanteId'],
+        mounted() {
+            this.estadoVacanteData = Number(this.estado)
+        },
+        data() {
+            return {
+                estadoVacanteData: null,
+
+            }
+        },
+        methods: {
+            cambiarEstado(){
+               if(this.estadoVacanteData === 1){
+                   this.estadoVacanteData = 0;
+               }else{
+                    this.estadoVacanteData = 1;
+               }
+            },
+            claseEstadoVacante(){
+                return this.estadoVacanteData === 1 ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'
+            }
+        },
+        computed: {
+            estadoVacante(){
+                return this.estadoVacanteData === 1 ? 'Activa' : 'Inactiva'
+            }
+        },
 
     }
 </script>
